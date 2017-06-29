d62827ede99e86d5e52490f2762b40f78cec4376
Jamie Player <jamieplayer@gmail.com>
2017-06-29 23:15:21 +0100

    updated to send json object and added PORT var

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require("express");
 var app = express();
 
-
 app.get("/", function(req, res) {
 
   var ua = req.headers['user-agent'];
   var host = req.headers['host'];
   var lang = req.headers['accept-language'];
   var ip = req.ip;
-  console.log(ip);
-  res.end();
+
+  res.json({
+    ip: ip,
+    software: ua,
+    language: lang
+  });
+
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 })
